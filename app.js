7a35f0d17b300c293fd2e2aa553c7c4116bf8001
sandeepsk2020 <sandeepsk2020@gmail.com>
2023-10-23 16:44:36 +0530

    parcel added

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
 const heading = React.createElement('div', {"id": "d1"}, [
 
     React.createElement('div',{id : "d11"}, [
         React.createElement('h1',{},'h1 read'),
         React.createElement('p',{},'p1 read')
     ]),
-    React.createElement('div',{id : "d11"}, [
+    React.createElement('div',{id : "d12"}, [
         React.createElement('h1',{},'h1 read'),
         React.createElement('p',{},'p1 read')
     ])
